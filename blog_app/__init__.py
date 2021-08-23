807430961c52466d7f32d7a23fdb47f5a35e142e
priscillahnalubega <nalubegapriscillah@gmail.com>
2021-08-23 10:52:16 +0300

    first commit

---
+++ blog_app/__init__.py
@@ -0,0 +1,24 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a73b5850c104ceb2d0edbb8c6f4aec9e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from blog_app.errors.handlers import errors 
+
+app.register_blueprint(errors)
+
+
+from blog_app import routes
+
